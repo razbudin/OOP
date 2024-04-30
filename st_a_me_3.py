2f5b007d1e7c9c1b2448fa44e2e4c996dcd45d9b
razbudin <razbudin@yandex.ru>
2024-04-30 18:57:28 +0300

    Задание 3, внесены поправки

--- st_a_me_3.py
+++ st_a_me_3.py
@@ -8,6 +8,15 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    # Средняя оценка за домашние задания
+    def homework_rating(self):
+        values_grade = []
+        for _, value in self.grades.items():
+            values_grade.extend(value)
+        if len(values_grade):
+            return round(sum(values_grade) / len(values_grade), 1)
+        return
+
     # Задание 2, метод для оценки студентами лекторов
     def evaluated(self, lecturer, course, grade):
         if (isinstance(lecturer, Lecturer) and
@@ -22,23 +31,26 @@ class Student:
 
     # Задание 3, __str__
     def __str__(self):
-        values_grade = []
-        for _, value in self.grades.items():
-            values_grade.extend(value)
-
-        if len(values_grade):
-            avg_grade = round(sum(values_grade) / len(values_grade), 1)
-        else:
-            avg_grade = ''
-
         study = str(self.courses_in_progress)
         finished = str(self.finished_courses)
         return (
             f'Имя: {self.name}\nФамилия: {self.surname}\n'
-            f'Средняя оценка за домашние задания: {avg_grade}\n'
+            f'Средняя оценка за домашние задания: {self.homework_rating()}\n'
             f'Курсы в процессе изучения: {study}\n'
             f'Завершенные курсы: {finished}'
         )
+
+    # Задание 3, сравнение через < >
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print('Объекты должны принадлежать классу Student')
+            return
+        if (self.homework_rating() is not None and
+                other.homework_rating is not None):
+            return (self.homework_rating() < other.homework_rating(),
+                    self.name, other.name)
+        else:
+            return 'Невозможно выполнить сравнение'
 
 
 class Mentor:
@@ -56,15 +68,31 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades = {}
 
-    # Задание 3, __str__
-    def __str__(self):
+    # Средняя оценка за лекции
+    def lection_rating(self):
         values_grade = []
         for _, value in self.grades.items():
             values_grade.extend(value)
-        grade = round(sum(values_grade) / len(values_grade), 1)
+        if len(values_grade):
+            return round(sum(values_grade) / len(values_grade), 1)
+
+    # Задание 3, __str__
+    def __str__(self):
         return (
             f'Имя: {self.name}\nФамилия:'
-            f'{self.surname}\nСредняя оценка за лекции: {grade}')
+            f'{self.surname}\nСредняя оценка за лекции: {self.lection_rating()}')
+
+    # Задание 3, сравнение через < >
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Объекты должны принадлежать классу  Lecturer')
+            return
+        if (self.lection_rating() is not None and
+                other.lection_rating() is not None):
+            return (self.lection_rating() < other.lection_rating(),
+                    self.name, other.name)
+        else:
+            return 'Невозможно выполнить сравнение'
 
 
 class Reviewer(Mentor):
@@ -91,42 +119,52 @@ class Reviewer(Mentor):
 
 
 # создание объектов класса
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
-best_student.courses_in_progress += ['Java']
+student1 = Student('Ruoy', 'Eman', 'your_gender')
+student1.courses_in_progress += ['Python']
+student1.courses_in_progress += ['Java']
 
 student2 = Student('Peter', 'Parcker', 'man')
 student2.courses_in_progress += ['Python']
 student2.courses_in_progress += ['Java']
 student2.finished_courses += ['C++']
 
-cool_lectur = Lecturer('Some', 'Buddy')
-cool_lectur.courses_attached += ['Python']
-cool_lectur.courses_attached += ['Java']
+lectur1 = Lecturer('Some', 'Buddy')
+lectur1.courses_attached += ['Python']
+lectur1.courses_attached += ['Java']
+lectur2 = Lecturer('Ivan', 'Bronevoy')
+lectur2.courses_attached += ['Python']
+lectur2.courses_attached += ['C++']
 
-cool_review = Reviewer('Wasija', 'Bugldy')
-cool_review.courses_attached += ['Python']
-cool_review.courses_attached += ['C++']
+review1 = Reviewer('Wasija', 'Bugldy')
+review1.courses_attached += ['Python']
+review1.courses_attached += ['C++']
 
-cool_review.rate_hw(best_student, 'Python', 10)
-cool_review.rate_hw(best_student, 'Python', 9)
-cool_review.rate_hw(best_student, 'C++', 10)
-cool_review.rate_hw(student2, 'Python', 9)
-cool_review.rate_hw(student2, 'Python', 8)
+review1.rate_hw(student1, 'Python', 10)
+review1.rate_hw(student1, 'Python', 9)
+review1.rate_hw(student1, 'C++', 10)
+review1.rate_hw(student2, 'Python', 9)
+review1.rate_hw(student2, 'Python', 8)
 
 
-student2.evaluated(cool_lectur, 'Python', 10)
-student2.evaluated(cool_lectur, 'Java', 9)
-best_student.evaluated(cool_lectur, 'Python', 10)
-best_student.evaluated(cool_lectur, 'Java', 10)
+student2.evaluated(lectur1, 'Python', 10)
+student2.evaluated(lectur1, 'Java', 9)
+student1.evaluated(lectur1, 'Python', 10)
+student1.evaluated(lectur2, 'Java', 10)
+student2.evaluated(lectur2, 'Python', 10)
+student2.evaluated(lectur1, 'Java', 9)
+student1.evaluated(lectur2, 'Python', 10)
+student1.evaluated(lectur2, 'Java', 10)
 
 
 # проверочные принты
-# print('Оценки студента', best_student.grades)
-# print('Лектор', cool_lectur.courses_attached)
-# print('Ревьювер', cool_review.courses_attached)
-# print('Изучаемые курсы', best_student.courses_in_progress)
-# print('Оценки лектора', cool_lectur.grades)
+# print('Оценки студента', student1.grades)
+# print('Лектор', lectur1.courses_attached)
+# print('Ревьювер', review1.courses_attached)
+# print('Изучаемые курсы', student1.courses_in_progress)
+# print('Оценки лектора', lectur1.grades)
 print('student str', student2, sep='\n')
-print('reviewer str', cool_review, sep='\n')
-print('lectur str', cool_lectur, sep='\n')
+print('reviewer str', review1, sep='\n')
+print('lectur str', lectur1, sep='\n')
+print(student1 < student2)
+print(lectur1 < lectur2)
+print(lectur2 < lectur1)
